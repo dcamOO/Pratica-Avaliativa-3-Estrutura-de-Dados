cd4d6b8cce12133f768b4dc0b6c9566c8e4bb844
dcamOO <112895322+dcamOO@users.noreply.github.com>
2025-05-16 17:04:10 -0300

    Update main.cpp
    first functional version

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,14 @@ int main(){
 
     int tamanho;
 
-    // std::cout << "Insira o tamanho do Heap: ";
-    // std::cin >> tamanho;
+    std::cout << "Insira o tamanho do Heap: ";
+    std::cin >> tamanho;
     Heap heap = Heap(tamanho);
-    // std::cout << "tamanho escolhido: " << tamanho << std::endl;
+    std::cout << "tamanho escolhido: " << tamanho << std::endl;
 
-    // std::cout << "Insira os " << tamanho << " elementos: " << std::endl;
+    std::cout << "Insira os " << tamanho << " elementos: " << std::endl;
     
+    std::cin.ignore();
     std::string linha;
     std::getline(std::cin, linha);
 
@@ -24,6 +25,11 @@ int main(){
     while(streamEntradas >> e){
         heap.Inserir(e);
     }
+
+    while(!heap.Vazio()){
+        heap.Remover();
+    }
+    std::cout << std::endl;
 
     return 0;
 }
