7517b0aa8a357f7507ac10c732f8b643f330a957
Daniel Canton <danielcanton@gmail.com>
2025-05-16 12:21:06 -0300

    untested heap implementation and starting main implementation

--- src/heap.cpp
+++ src/heap.cpp
@@ -1,74 +1,88 @@
-#include "heap.hpp"
+#include "../include/heap.hpp"
 #include <iostream>
 
 Heap::Heap(int maxsize){
-    /*
-    vetor heap[maxsize];
-    */
+    this->tamanho = 0;
+    this->data = new int[maxsize];
 }
 
 Heap::~Heap(){
-/*
-    troca primeiro e último valor
-    remove o último
-    heapify
-    troca o primeiro e último...
-*/
+    delete[] data; 
 }
 
 void Heap::Inserir(int x){
-    /*
-    insere elemento no final
-    heapify
-    */
+    //insere elemento no final
+    this->tamanho++;
+    data[tamanho-1] = x;
+    
+    
+    //reorganiza heap
+    HeapifyPorBaixo(tamanho-1);
 }
 int Heap::Remover(){
-/*
-troca elemento a ser removido com o último
-heapify
-*/
+    //troca elemento a ser removido com o último
+    if(tamanho == 0){
+        return 1;
+    }
+
+    int aux = data[tamanho-1];
+    data[tamanho-1] = data[0];
+    data[0] = aux;
+    this->tamanho--;
+
+    if(tamanho == 0){
+        return 1;
+    }
+
+    //reorganiza heap
+    HeapifyPorCima(tamanho-1);
+
+    return 0;
 }
 
 //Retorna true caso o heap esteja vazio, false caso contrário.
 bool Heap::Vazio(){
-    /*
-    verifica se raiz existe
-    */
+    //verifica se a raiz existe
+    return this->tamanho == 0? true : false;
 }
 
 int Heap::GetAncestral(int posicao){
-    /*
-    return vetor[(posicao - 1) / 2];
-    */
+    return data[(posicao - 1) / 2];
 }
 int Heap::GetSucessorEsq(int posicao){
-    /*
-    return vetor[posicao*2+1];
-    */
+    return data[posicao*2+1];
 }
 int Heap::GetSucessorDir(int posicao){
-    /*
-    return vetor[posicao*2+2];
-    */
+    return data[posicao*2+2];
 }
-
 
 void Heap::HeapifyPorBaixo(int posicao){
-/*
-    if(GetAncestral(posicao) > vetor[posicao]){
-        swap ancestral e vetor[posicao];
+    if(GetAncestral(posicao) > data[posicao]){
+        //troca pai e filho de lugar
+        int aux = data[posicao];
+        data[posicao] = GetAncestral(posicao);
+        data[(posicao - 1) / 2] = aux;
+
+        //chama heapify denovo recursivamente
         HeapifyPorBaixo((posicao - 1) / 2);
     }
-*/
 }
 void Heap::HeapifyPorCima(int posicao){
-    /*
-    GetSucessorEsq(posicao) = e;
-    GetSucessorDir(posicao) = d;
-    vetor[posicao] = c;
+    int e = GetSucessorEsq(posicao);
+    int d = GetSucessorDir(posicao);
+    int c = data[posicao];
 
-    if(e < c || d < c){
-        e < d? swap(e,c) : swap(d,c);
+    if(e < c || d < c){ //sucessores são menores?
+        if(e <= d){ //qual sucessor é menor?
+            int aux = e;
+            e = c;
+            c = aux;
+            HeapifyPorCima(posicao*2+2);
+        }else if(d < e){
+            int aux = d;
+            d = c;
+            c = aux;
+            HeapifyPorCima(posicao*2+1);
+        }
     }
-    */
 }
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "../include/heap.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+int main(){
+
+    int tamanho;
+
+    // std::cout << "Insira o tamanho do Heap: ";
+    // std::cin >> tamanho;
+    Heap heap = Heap(tamanho);
+    // std::cout << "tamanho escolhido: " << tamanho << std::endl;
+
+    // std::cout << "Insira os " << tamanho << " elementos: " << std::endl;
+    
+    std::string linha;
+    std::getline(std::cin, linha);
+
+    std::stringstream streamEntradas(linha);
+
+    int e;
+    while(streamEntradas >> e){
+        heap.Inserir(e);
+    }
+
+    return 0;
+}
